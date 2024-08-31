7c7f4b3a1e36044f2964fc50701119195c6a4d74
Yuvraj Singh <113302796+Yuvrajsinghspd09@users.noreply.github.com>
2024-08-31 18:49:36 +0530

    Update Data Collection.py

--- Data Collection.py
+++ Data Collection.py
@@ -1,29 +1,37 @@
+# 2. Data Collection
 class TwitterDataCollector:
-  def __init__(self, api, max_tweets, output_file):
-    self.api = api
-    self.max_tweets = max_tweets
-    self.output_file = output_file
-    
+    def __init__(self, api, max_tweets, output_file):
+        self.api = api
+        self.max_tweets = max_tweets
+        self.output_file = output_file
+
     def collect_sample_stream(self):
-      class SampleStreamListener(tweepy.StreamListener):
-        def __init__(self, max_tweets, output_file):
-          self.max_tweets = max_tweets
-          self.tweet_count = 0
-          self.output_file = output_file
+        class SampleStreamListener(tweepy.Stream):
+            def __init__(self, api_key, api_secret_key, access_token, access_token_secret, max_tweets, output_file):
+                super().__init__(api_key, api_secret_key, access_token, access_token_secret)
+                self.max_tweets = max_tweets
+                self.tweet_count = 0
+                self.output_file = output_file
 
-        def on_data(self, data):
-          if self.tweet_count<self.max_tweets :
-            with open(self.output_file, 'a') as f:
-              f.write(data)
-            self.tweet_count+=1
-            return True
-          else:
-            return False
+            def on_data(self, data):
+                if self.tweet_count < self.max_tweets:
+                    with open(self.output_file, 'a') as f:
+                        f.write(data)
+                    self.tweet_count += 1
+                    return True
+                else:
+                    return False
 
-        def on_error(self,status):
-          print(f"Error: {status}")
+            def on_error(self, status):
+                print(f"Error: {status}")
+                return True
 
-      listener = SampleStreamListener()
-      stream = tweepy.Stream(auth = self.api.auth, listener = listener)
-      stream.sample(languages=['en'])
-
+        listener = SampleStreamListener(
+            self.api.auth.consumer_key,
+            self.api.auth.consumer_secret,
+            self.api.auth.access_token,
+            self.api.auth.access_token_secret,
+            self.max_tweets,
+            self.output_file
+        )
+        listener.filter(languages=['en'])
