7289d2c80e98a92947aff6522799edb5b40e2c05
Yuvraj Singh <113302796+Yuvrajsinghspd09@users.noreply.github.com>
2024-08-31 14:06:08 +0530

    Update Data Collection.py

--- Data Collection.py
+++ Data Collection.py
@@ -1,1 +1,29 @@
+class TwitterDataCollector:
+  def __init__(self, api, max_tweets, output_file):
+    self.api = api
+    self.max_tweets = max_tweets
+    self.output_file = output_file
+    
+    def collect_sample_stream(self):
+      class SampleStreamListener(tweepy.StreamListener):
+        def __init__(self, max_tweets, output_file):
+          self.max_tweets = max_tweets
+          self.tweet_count = 0
+          self.output_file = output_file
+
+        def on_data(self, data):
+          if self.tweet_count<self.max_tweets :
+            with open(self.output_file, 'a') as f:
+              f.write(data)
+            self.tweet_count+=1
+            return True
+          else:
+            return False
+
+        def on_error(self,status):
+          print(f"Error: {status}")
+
+      listener = SampleStreamListener()
+      stream = tweepy.Stream(auth = self.api.auth, listener = listener)
+      stream.sample(languages=['en'])
 
